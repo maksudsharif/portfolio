1cd571cce37202883c5cad48f836daea64c0ec7d
Maksud Sharif <maksudsharif@gmail.com>
2021-01-23 11:07:13 -0500

    Angular 11 & Spring Boot 2.4.2

--- portfolio-client/src/app/components/portfolio-body/portfolio-body.component.spec.ts
+++ portfolio-client/src/app/components/portfolio-body/portfolio-body.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PortfolioBodyComponent } from './portfolio-body.component';
 
@@ -6,7 +6,7 @@ describe('PortfolioBodyComponent', () => {
   let component: PortfolioBodyComponent;
   let fixture: ComponentFixture<PortfolioBodyComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PortfolioBodyComponent ]
     })
